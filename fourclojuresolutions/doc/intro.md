78b39f5af749ce1da4243a3d3690db18b85fab36
Michael J. Cusack <whoneedszzz@gmail.com>
2018-01-08 17:00:27 -0600

    Base Leiningen app

---
+++ fourclojuresolutions/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fourclojuresolutions
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ fourclojuresolutions/test/fourclojuresolutions/core_test.clj
@@ -0,0 +1,7 @@
+(ns fourclojuresolutions.core-test
+  (:require [clojure.test :refer :all]
+            [fourclojuresolutions.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
