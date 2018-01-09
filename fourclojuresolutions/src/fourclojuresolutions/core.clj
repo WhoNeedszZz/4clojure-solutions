7312fe492f8286d2a0bb7b9c13180fa6e51e146d
Michael J. Cusack <whoneedszzz@gmail.com>
2018-01-08 18:33:58 -0600

    Easy - Nth Element

--- fourclojuresolutions/src/fourclojuresolutions/core.clj
+++ fourclojuresolutions/src/fourclojuresolutions/core.clj
@@ -12,7 +12,12 @@
     (println "Penultimate")
     (println (easy/penultimate (list 1 2 3 4 5)))
     (println (easy/penultimate ["a" "b" "c"]))
-    (println (easy/penultimate [[1 2] [3 4]]))))
+    (println (easy/penultimate [[1 2] [3 4]]))
+    (println "Nth element")
+    (println (easy/nthEl '(4 5 6 7) 2))
+    (println (easy/nthEl [:a :b :c] 0))
+    (println (easy/nthEl [1 2 3 4] 1))
+    (println (easy/nthEl '([1 2] [3 4] [5 6]) 2))))
 
 (defn -main
   "4Clojure solutions"
--- fourclojuresolutions/src/fourclojuresolutions/easy.clj
+++ fourclojuresolutions/src/fourclojuresolutions/easy.clj
@@ -10,3 +10,8 @@
   "Write a function which returns the second to last element from a sequence"
   [sequ]
   (first (rest (reverse sequ))))
+
+(defn nthEl
+  "Write a function which returns the Nth element from a sequence"
+  ([sequ n] (if (= n 0) (nthEl sequ) (nthEl (rest sequ) (- n 1))))
+  ([sequ] (first sequ)))
