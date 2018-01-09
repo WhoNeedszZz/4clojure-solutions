a163c3b7b10a4b431e0775ba791f45e98c63869a
Michael J. Cusack <whoneedszzz@gmail.com>
2018-01-08 18:04:06 -0600

    Easy - penultimate

--- fourclojuresolutions/src/fourclojuresolutions/core.clj
+++ fourclojuresolutions/src/fourclojuresolutions/core.clj
@@ -8,7 +8,11 @@
     (println "Last element")
     (println (easy/lastEl [1 2 3 4 5]))
     (println (easy/lastEl '(5 4 3)))
-    (println (easy/lastEl ["b" "c" "d"]))))
+    (println (easy/lastEl ["b" "c" "d"]))
+    (println "Penultimate")
+    (println (easy/penultimate (list 1 2 3 4 5)))
+    (println (easy/penultimate ["a" "b" "c"]))
+    (println (easy/penultimate [[1 2] [3 4]]))))
 
 (defn -main
   "4Clojure solutions"
--- fourclojuresolutions/src/fourclojuresolutions/easy.clj
+++ fourclojuresolutions/src/fourclojuresolutions/easy.clj
@@ -5,3 +5,8 @@
   "Write a function which returns the last element in a sequence"
   [sequ]
   (first (reverse sequ)))
+
+(defn penultimate
+  "Write a function which returns the second to last element from a sequence"
+  [sequ]
+  (first (rest (reverse sequ))))
